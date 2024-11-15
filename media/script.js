9ee9138c722a204610890cc619f0378f3a494a3a
Ben Hagag <benbenhagag@gmail.com>
2024-11-15 15:02:08 +0200

    WIP

--- media/script.js
+++ media/script.js
@@ -115,19 +115,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Predefined images from your project (example)
     const projectImages = [
-        '../images/1.jpeg',
-        '../images/2.jpeg',
-        '../images/3.jpeg',
-        '../images/4.jpeg',
-        '../images/5.jpeg',
-        '../images/6.jpeg',
-        '../images/7.jpeg',
-        '../images/8.jpeg',
-        '../images/9.jpeg',
-        '../images/10.jpeg',
-        '../images/11.jpeg',
-        '../images/12.jpeg',
-        '../images/13.jpeg',
+        'images/1.jpeg',
+        'images/2.jpeg',
+        'images/3.jpeg',
+        'images/4.jpeg',
+        'images/5.jpeg',
+        'images/6.jpeg',
+        'images/7.jpeg',
+        'images/8.jpeg',
+        'images/9.jpeg',
+        'images/10.jpeg',
+        'images/11.jpeg',
+        'images/12.jpeg',
+        'images/13.jpeg',
     ];
 
     // Upload Handler
